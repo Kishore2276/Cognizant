56829e2062b230d9c45d8ffb87f85a1e67769107
Kishore2276 <skishorebose@gmail.com>
2026-07-17 19:01:03 +0530

    feat: Implement initial Flask app structure with models, routes, and templates

---
+++ PythonBackend/HandsOn4/app/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+class Course(db.Model):
+    __tablename__ = "courses"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Course {self.name}>"
