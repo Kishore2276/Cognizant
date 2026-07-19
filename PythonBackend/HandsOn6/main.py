fd1947b17b2711dfe72c5abeccc4ca537fa459f9
Kishore2276 <skishorebose@gmail.com>
2026-07-19 21:17:29 +0530

    feat: Implement database setup and add course retrieval endpoints in FastAPI

--- PythonBackend/HandsOn6/main.py
+++ PythonBackend/HandsOn6/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_db
 from schemas import CourseCreate
+from typing import Optional
 
 app = FastAPI(
     title="Course Management API",
@@ -15,4 +18,23 @@ async def create_course(course: CourseCreate):
     return {
         "message": "Course created successfully",
         "course": course
+    }
+
+@app.get("/api/courses/{course_id}")
+async def get_course(course_id: int):
+    return {
+        "course_id": course_id,
+        "message": "Course Found"
+    }
+
+@app.get("/api/courses/")
+async def get_courses(
+    skip: int = 0,
+    limit: int = 10,
+    department_id: Optional[int] = None
+):
+    return {
+        "skip": skip,
+        "limit": limit,
+        "department_id": department_id
     }
